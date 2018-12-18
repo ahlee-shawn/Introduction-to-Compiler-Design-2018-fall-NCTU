553cfea2cd18189ed599d777c6f9412fc89a6138
Ang-Hsuan Lee <leeang6969.cs05@g2.nctu.edu.tw>
2018-12-18 13:06:02 +0800

    Upload Lab2

---
+++ Lab2/table.java
@@ -0,0 +1,11 @@
+import java.io.*; 
+import java.util.*; 
+
+public class table
+{
+	public static Vector<String> int_vec = new Vector<String>(); 
+    public static Vector<String> bool_vec = new Vector<String>();  
+    public static Vector<String> String_vec = new Vector<String>();
+    public static Vector<String> error_vec = new Vector<String>();
+    public static int error = 0;
+ };
